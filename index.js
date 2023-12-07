63d911b9763880a40636fc28ca98e2f5364141a8
Subu <subramanya4shenoy@gmail.com>
2023-12-07 14:47:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,6 +69,71 @@ app.post("/generate", async (req, res) => {
   }
 });
 
+app.post("/generateAll", async (req, res) => {
+  console.log("Required type:", req.body.type);
+  console.log("Entered keywords:", req.body.keywords);
+  console.log("Entered tone:", req.body.tone);
+
+  let  prompt1 = `Generate a ${req.body.tone} caption (up to 250 characters) without hashtags and also generate relevant hashtags separately for an Instagram post featuring ${req.body.keywords}. Format the result in JSON strictly as follows: {"data": "", "hashtag": ""}`;
+  let  prompt2 = `Generate a ${req.body.tone} first comment without hashtags for an Instagram post featuring ${req.body.keywords}. Format the result in JSON strictly as follows: {"data": ""}`;
+  let  prompt3 = `Generate a title for a Pinterest post featuring ${req.body.keywords}. Format the result in JSON strictly as follows: {"data": ""}`;
+
+try {
+  const prompts = [prompt1, prompt2, prompt3]; // replace with your actual prompts
+  const requests = prompts.map((prompt, index) =>
+    axios
+      .post(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "system",
+              content: "You are a helpful assistant.",
+            },
+            {
+              role: "user",
+              content: prompt,
+            },
+          ],
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.OPENAI_APIKEY}`,
+          },
+        }
+      )
+      .then((response) => {
+        console.log("Response:", response.data)
+        let content;
+        try {
+          content = JSON.parse(response.data.choices[0].message.content);
+        } catch (error) {
+          console.error(
+            "Invalid JSON:",
+            response.data.choices[0].message.content
+          );
+          content = response.data.choices[0].message.content;
+        }
+        return { [`data${index + 1}`]: content };
+      })
+      .catch((error) => {
+        console.error(
+          "Error:",
+          error.response ? error.response.data : error.message
+        );
+        return { [`data${index + 1}`]: null };
+      })
+  );
+
+  const responses = await Promise.all(requests);
+  const data = Object.assign({}, ...responses);
+  res.send({ data });
+} catch (error) {
+  console.error("Error:", error.response ? error.response.data : error.message);
+  res.status(500).send("An error occurred while generating");
+}
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
